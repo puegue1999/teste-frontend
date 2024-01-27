bdcae6a684dcf1c6a4fdeeebe6d7def030ff5b13
puegue1999 <ra-fasilva2010@hotmail.com>
2024-01-27 18:25:18 -0300

    Feat: Listagem apenas com quatro elementos e possível agora a paginação

--- src/app/component/list/list.component.ts
+++ src/app/component/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -7,8 +7,49 @@ import { Component, Input } from '@angular/core';
 })
 export class ListComponent {
   @Input() dados: any;
+  @Input() filtro: any;
+  @Input() fechadas: any;
+  @Input() pagina: any;
+  @Output() mudarPagina: EventEmitter<number> = new EventEmitter<number>();
+  itensPorPagina = 4;
+  desktop = true;
+  total: any;
+  paginas: any[] = [];
 
-  get dadosSorted(): any[] {
-    return this.dados && this.dados.slice().sort((a: { id: number; }, b: { id: number; }) => a.id - b.id);
+  ngOnInit() {
+    if (this.larguraTela < 768) {
+      this.itensPorPagina = 1;
+      this.desktop = false;
+    }
+
+  }
+
+  // Itens listados por página;
+  get itensNaPagina() {
+    if (this.dados) {
+      this.paginas = [];
+      this.total = Math.ceil(this.dados.length/this.itensPorPagina);
+      for (let i = 0; i < this.dados.length; i += this.itensPorPagina) {
+        this.paginas.push(this.dados.slice(i, i + this.itensPorPagina));
+      }
+    }
+    return this.paginas[this.pagina];
+  }
+
+  get larguraTela(): number {
+    return window.innerWidth;
+  }
+
+  eventMudarPagina(paginaAtual: number): void {
+    this.mudarPagina.emit(paginaAtual);
+  }
+
+  // Função necessária pois o endereço está em formato html;
+  textoFormatado(textoBruto: string): string {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(textoBruto, 'text/html');
+    const body = doc.body;
+
+    return body.innerHTML;
   }
 }
